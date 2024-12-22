9962e23cd0993b20783fc2ff571744071c2c8aaf
Marden Melo <mardenmelo1@gmail.com>
2024-12-22 00:53:57 -0300

    feat: models

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import { app } from "./app";
+import { app } from './app';
+import { env } from './env';
 
 app
   .listen({
-    port: 3333,
-    host: "0.0.0.0",
+    port: env.PORT,
+    host: '0.0.0.0',
   })
   .then(() => {
-    console.log("HTTP Server Running");
+    console.log('HTTP Server Running');
   });
